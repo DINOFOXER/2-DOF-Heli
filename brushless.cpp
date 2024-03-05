e87798f9b14e1a2fa8bce17f4ef5b1e07d0fcbf9
Willian de Oliveira Silva <50004325+DINOFOXER@users.noreply.github.com>
2024-03-04 22:47:37 -0300

    Reupload de arquivos
    Acabei deletando sem querer de minha pasta pessoal. Pelo menos os avanços foram tímidos.

---
+++ brushless.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "pigpio.h"
+
+using namespace std;
+
+int main{
+	gpioInitialize();
+	gpioSetMode(17, PI_OUTPUT);
+	
+	cout << "Ligando motor..." << endl;
+	gpioServo(17, 1000);
+	time_sleep(5);
+	
+	cout << "Acelerando motor" << endl;
+	gpioServo(17, 1500);
+	time_sleep(2);
+	
+	cout << "Desligando motor...." << endl;
+	gpioServo(17, 1000);
+	time_sleep(1)
+	
+	
+	gpioTerminate();
+	return 0;	
+}
