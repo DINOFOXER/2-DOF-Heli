fc85215eb2defa07a19ff2cee150e59b4819a55a
Willian de Oliveira Silva <50004325+DINOFOXER@users.noreply.github.com>
2024-03-04 23:03:58 -0300

    Setting up code structure

--- bno055/bno.cpp
+++ bno055/bno.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include "pigpio.h"
 
+using namespace std;
+
+#define xDegH = 0x1B
+#define xDegL = 0x1A
+#define bno = 0x29
+
+int handle = 0;
+int16_t xDegree = 0;
+
+
+
+int main () {
+
+	gpioInitialise();
+  handle = i2cOpen(1, bno, 0);
+
+  i2cClose(handle);
+  gpioTerminate();
+  return 0;
+}
+	
+	
